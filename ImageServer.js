dae5fdd8c1c591be859217bc68043f2e2c4dd708
Joaquim-Chambuara <122440707+Joaquim-Chambuara@users.noreply.github.com>
2023-05-27 21:47:24 +0200

    Update ImageServer.js

--- ImageServer.js
+++ ImageServer.js
@@ -1,5 +1,5 @@
 require('dotenv').config();
-const port = process.env.PORT || 3000;
+const port = process.env.PORT;
 const apikey = process.env.PEXELS_API_KEY;
 const express = require('express');
 const http = require('http');
@@ -8,20 +8,21 @@ const axios = require('axios');
 
 const app = express();
 app.use(cors());
+
 app.get('/api/images', async (req, res) => {
-    const url = `https://api.pexels.com/v1/search?query=${req.query.query}&page=${req.query.page}&per_page=${req.query.per_page}`;
-    try {
-      const response = await axios.get(url, {
-        headers: {
-          Authorization: apikey
-        }
-      });
-      console.log(response.data.photos);
-      res.json(response.data.photos);
-    } catch (error) {
-      console.error(`Error: ${error}`);
-      res.status(500).json({ message: 'Server error' });
-    }
+  const url = `https://api.pexels.com/v1/search?query=${req.query.query}&page=${req.query.page}&per_page=${req.query.per_page}`;
+  try {
+    const response = await axios.get(url, {
+      headers: {
+        Authorization: apikey
+      }
+    });
+    console.log(response.data.photos);
+    res.json(response.data.photos);
+  } catch (error) {
+    console.error(`Error: ${error}`);
+    res.status(500).json({ message: 'Server error' });
+  }
 });
 
 const server = http.createServer(app);
